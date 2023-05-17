699028b230e16548ea225c923760aef1c6f8fe2b
tasevskinaum <tasevskinaum@yahoo.com>
2023-05-17 16:07:37 +0200

    test3

--- assets/js/hire-form.js
+++ assets/js/hire-form.js
@@ -33,19 +33,32 @@ function onload_form_container_height() {
 }
 
 function set_form_container_height() {
+
+    if (window.innerHeight < 768 && window.innerWidth >= 768) {
+        header.style.padding = "3rem 3rem"
+    }
+    else if (window.innerWidth <= 768) {
+        header.style.padding = "1.875rem 0rem"
+    }
+    else {
+        header.style.padding = "5.625rem 0rem"
+    }
+
     var navbar_height = navbar.offsetHeight;
     var header_height = header.offsetHeight;
     var footer_height = footer.offsetHeight;
     var type_std_labels_height = type_std_labels.offsetHeight;
     var view_height = window.innerHeight;
 
-    var form_height = (view_height + 150)
+
+
+    var form_height = (view_height)
         - (parseInt(navbar_height)
             + parseInt(header_height)
             + parseInt(footer_height))
         + parseInt(type_std_labels_height);
 
-    form_container.style.height = (form_height + "px");
+    form_container.style.minHeight = (form_height + "px");
 }
 
 function toggleHiden() {
