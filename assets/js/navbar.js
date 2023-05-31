c2dd39f084b40581a55d8a002b10e11b613811ec
tasevskinaum <tasevskinaum@yahoo.com>
2023-05-31 15:59:52 +0200

    test10

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,11 +1,41 @@
-const menuWrapper = document.querySelector(".menu-wrapper");
-const navbarToggler = document.querySelector(".navbar-toggler");
-const closeBtn = document.querySelector(".close-btn");
+const navbar = document.querySelector("nav.navbar");
+const navbar_toggler = document.querySelector(".navbar-toggler");
+const menu_wrapper = document.querySelector(".navbar .menu-wrapper");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
 
-navbarToggler.addEventListener("click", () => {
-  menuWrapper.classList.add("show");
-});
+window.onload = function () {
+    set_menu_wrapper_top_position();
+    set_body_top_position();
+    set_header_height();
+}
 
-closeBtn.addEventListener("click", () => {
-  menuWrapper.classList.remove("show");
-});
+window.addEventListener("resize", () => {
+    set_menu_wrapper_top_position();
+    set_body_top_position();
+    set_header_height();
+})
+
+
+// navbar
+navbar_toggler.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    menu_wrapper.classList.toggle("active");
+    navbar_toggler.classList.toggle("active");
+})
+
+function set_menu_wrapper_top_position() {
+    var navbar_height = navbar.offsetHeight;
+    menu_wrapper.style.top = parseInt(navbar_height) + "px";
+}
+
+function set_body_top_position() {
+    var navbar_height = navbar.offsetHeight;
+    body.style.paddingTop = parseInt(navbar_height) + "px";
+}
+
+
+// header
+function set_header_height() {
+    header.style.height = window.innerHeight - navbar.offsetHeight + "px";
+}
